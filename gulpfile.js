cff2d8b86865e83acf05cdc9c79a8e9b0ae6a924
hwilsmann <hwilsmann@sesiom.com.br>
2024-12-17 16:45:35 -0300

    Soma Prag

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const clean_css = require('gulp-clean-css');
+const html_min = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+async function load_imagemin() {
+    const image_min = await import('gulp-imagemin');
+    return image_min.default;
+}
+
+function css() {
+    return src('src/scss/**/*.scss')
+        .pipe(plumber({
+            errorHandler: function(err) {
+                console.error('Erro no CSS:', err.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(clean_css())
+        .pipe(concat('style.min.css'))
+        .pipe(dest('dist/css'))
+        .pipe(connect.reload())
+        .on('end', () => console.log('CSS compilado e minificado!'));
+}
+
+function html() {
+    return src('src/*.html')
+        .pipe(plumber({
+            errorHandler: function(err) {
+                console.error('Erro no HTML:', err.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(html_min({ collapseWhitespace: true }))
+        .pipe(dest('dist'))
+        .pipe(connect.reload())
+        .on('end', () => console.log('HTML minificado com sucesso!'));
+}
+
+function js() {
+    return src('src/js/*.js')
+        .pipe(plumber({
+            errorHandler: function(err) {
+                console.error('Erro no JavaScript:', err.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(concat('script.min.js'))
+        .pipe(dest('dist/js'))
+        .pipe(connect.reload())
+        .on('end', () => console.log('JavaScript compilado e minificado!'));
+}
+
+async function images() {
+    const image_min = await load_imagemin();
+    return src('src/image/*')
+        .pipe(image_min({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true,
+            multipass: true
+        }))
+        .pipe(dest('dist/image'))
+        .on('end', () => console.log('Imagens otimizadas com sucesso!'));
+}
+
+function serve(done) {
+    connect.server({
+        root: 'dist',
+        livereload: true,
+        port: 8080
+    });
+
+    console.log('Servidor iniciado em http://localhost:8080');
+
+    watch('src/scss/**/*.scss', css);
+    watch('src/*.html', html);
+    watch('src/js/*.js', js);
+    watch('src/image/*', images);
+
+    done();
+}
+
+exports.default = series(parallel(css, html, js, images));
+exports.dev = series(parallel(css, html, js, images), serve);
---
+++ src/js/script.js
@@ -0,0 +1,73 @@
+function init_global() {
+  // Função do Popup
+  const open_buttons = document.querySelectorAll('.-open-popup');
+  const close_buttons = document.querySelectorAll('.-close-popup');
+
+  open_buttons.forEach(function (button) {
+    button.onclick = function (event) {
+      event.preventDefault();
+
+      const target = this.getAttribute('data-target');
+      const popup = document.getElementById(target);
+      if (popup) { popup.classList.add('-open'); }
+    }
+  });
+
+  close_buttons.forEach(function (button) {
+    button.onclick = function () {
+      const popup = this.closest('.popup');
+      if (popup) { popup.classList.remove('-open'); }
+    }
+  });
+
+  function open_popup(popup_id) {
+    const popup = document.getElementById(popup_id);
+    if (popup) { popup.classList.add('-open'); }
+  }
+
+  function close_popup(popup_id) {
+    const popup = document.getElementById(popup_id);
+    if (popup) { popup.classList.remove('-open'); }
+  }
+
+  // Função do Loading
+  document.body.classList.add('-loading');
+
+  const hide_loading = () => {
+    const loading = document.querySelector('.loading');
+    if (loading) {
+      loading.classList.add('-hidden');
+      loading.addEventListener('transitionend', () => {
+        document.body.classList.remove('-loading');
+      }, { once: true });
+    }
+  };
+
+  setTimeout(hide_loading, 1000);
+
+  // Função do Menu
+  const menu_wrappers = document.querySelectorAll('.menu-wrapper');
+  if (menu_wrappers.length > 0) {
+    menu_wrappers.forEach((menu_wrapper) => {
+      const button = menu_wrapper.querySelector('#menu');
+      if (button) {
+        button.addEventListener('click', () => {
+          menu_wrapper.classList.toggle('-open');
+        });
+      }
+    });
+  }
+}
+
+function init_survey() {}
+
+document.addEventListener('DOMContentLoaded', function () {
+  init_global();
+
+  const page = document.body.dataset.page;
+  if (page === 'survey') {
+    init_survey();
+  } else if (page === '') {
+  } else {
+  }
+});
